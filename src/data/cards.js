aa4063ae5e7a06d02cdce671d1e342978a60cb28
moustafa abdelati <mustafaabdelati@gmail.com>
2026-01-19 21:33:25 +1100

    uploaded assets - images - created cards.js data file

---
+++ src/data/cards.js
@@ -0,0 +1,27 @@
+// cards.js
+// This file defines all possible card TYPES in the game
+
+const suits = ["clubs", "diamonds", "hearts", "spades"];
+const ranks = [
+  { value: 2, label: "2" },
+  { value: 3, label: "3" },
+  { value: 4, label: "4" },
+  { value: 5, label: "5" },
+  { value: 6, label: "6" },
+  { value: 7, label: "7" },
+  { value: 8, label: "8" },
+  { value: 9, label: "9" },
+  { value: 10, label: "10" },
+  { value: 11, label: "J" },
+  { value: 12, label: "Q" },
+  { value: 13, label: "K" },
+  { value: 14, label: "A" },
+];
+
+export const CARD_TYPES = suits.flatMap((suit) =>
+  ranks.map((rank) => ({
+    id: `${rank.label}_of_${suit}`,
+    rank: rank.value,
+    // suit,
+  })),
+);
